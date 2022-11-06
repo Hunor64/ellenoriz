7433b716475b00fb3eab5223aecf5b477dd7fd62
Hunor József Nagy <Nhunor06@gmail.com>
2022-11-06 20:05:28 +0100

    Pablo 🐎

---
+++ script.js
@@ -0,0 +1,30 @@
+function elkuld()
+    {
+        document.getElementById('nevv').innerHTML = "Név: " + document.getElementById('nev').value;
+        document.getElementById('cimv').innerHTML = "Cím: " + document.getElementById('cim').value;
+        document.getElementById('szamv').innerHTML = "Telefon: " + document.getElementById('szam').value;
+    }
+function nevin()
+    {
+        document.getElementById("nev").style.backgroundColor = "blue";
+    }
+function nevout()
+    {
+        document.getElementById("nev").style.backgroundColor = "white";
+    }
+function cimin()
+    {
+        document.getElementById("cim").style.backgroundColor = "blue";
+    }
+function cimout()
+    {
+        document.getElementById("cim").style.backgroundColor = "white";
+    }
+function szamin()
+    {
+        document.getElementById("szam").style.backgroundColor = "blue";
+    }
+function szamout()
+    {
+        document.getElementById("szam").style.backgroundColor = "white";
+    }
